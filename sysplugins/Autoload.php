0112f5b0309a329b1f9622b6f1518945dc9039f3
mafer18062 <62572695+mafer18062@users.noreply.github.com>
2021-02-04 19:17:01 -0600

    otra modificacion
    nuevo

--- sysplugins/Autoload.php
+++ sysplugins/Autoload.php
@@ -4,10 +4,14 @@
     {
         if (is_file ('Controller/'.$class.'.Controller.php'))
         {
-            
+            require_once ('Controller/'.$class.'.Controller.php');
+        }
+        else if (is_file ('Framework/Smarty/'.$class.'.class.php'))
+        {
+            require_once ('Framework/Smarty/'.$class.'.class.php');
+        }
+    }
 
-
-
-
+    spl_autoload_register ('Autoload.php');
 
 ?>
